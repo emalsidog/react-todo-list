54d3b227b959f46cc64253d5f995a7afb64fbb50
Maksym <babiimaksym@gmail.com>
2021-03-02 12:48:21 +0200

    folders (server side)

--- server/controllers/folder.js
+++ server/controllers/folder.js
@@ -1,22 +1,221 @@
 // Dependencies
 const User = require("../models/User");
 const Folder = require("../models/Folder");
+const Todo = require("../models/Todo");
 
 // GET => /
 exports.getFolders = async (req, res) => {
-  const user = await User.findOne({ _id: req.user._id }).populate("folders").exec();
-  console.log(user);
-  // Send folders to client if no error
-  // If error => send error to user
-}
+  try {
+    const user = await User.findOne({ _id: req.user._id })
+      .populate("folders")
+      .exec();
+    res.status(200).json({
+      isError: false,
+      message: "Operation successfull.",
+      body: {
+        folders: user.folders,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
 
 // POST => /create-folder
-exports.postCreateFolder = (req, res) => {
+exports.postCreateFolder = async (req, res) => {
   const { title } = req.body;
   const folder = new Folder({
-    title
+    title,
   });
-  // Save folder
-  // Push folder to user`s folder array
-  // Save user
+  try {
+    await folder.save();
+    req.user.folders.push(folder);
+    await req.user.save();
+    res.status(200).json({
+      isError: false,
+      message: "Folder created.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
+
+// GET => /delete-folder
+exports.getDeleteFolder = async (req, res) => {
+  const folderId = req.params.id;
+  req.user.folders = req.user.folders.filter(({ _id }) => _id.toString() !== folderId.toString());
+  try {
+    await req.user.save();
+    await Folder.findOneAndDelete({ _id: folderId });
+
+    res.status(200).json({
+      isError: false,
+      message: "Operation successfull."
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
 }
+
+// GET => /:id
+exports.getTodos = async (req, res) => {
+  const folderId = req.params.id;
+
+  checkIfUserHasFolder(folderId, req.user, res);
+
+  try {
+    const { todos, title } = await Folder.findOne({ _id: folderId })
+      .populate("todos")
+      .exec();
+    res.status(200).json({
+      isError: false,
+      message: "Operation successfull.",
+      body: {
+        todos,
+        title,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
+
+// POST => /:id/add-todo
+exports.postAddTodo = async (req, res) => {
+  const { content } = req.body;
+  const folderId = req.params.id;
+
+  checkIfUserHasFolder(folderId, req.user, res);
+
+  if (!content) {
+    return res.status(400).json({
+      isError: true,
+      message: "Todo cannot be empty",
+    });
+  }
+
+  const todo = new Todo({
+    content,
+  });
+
+  try {
+    await todo.save();
+
+    const folder = await Folder.findOne({ _id: folderId });
+    folder.todos.push(todo);
+    await folder.save();
+
+    res.status(200).json({
+      isError: false,
+      message: "Successfully added todo.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
+
+// POST => /:id/delete-todo
+exports.postDeleteTodo = async (req, res) => {
+  const { id } = req.body;
+  const folderId = req.params.id;
+
+  checkIfUserHasFolder(folderId, req.user, res);
+
+  try {
+    const folder = await Folder.findOne({ _id: folderId });
+    folder.todos = folder.todos.filter(
+      ({ _id }) => _id.toString() !== id.toString()
+    );
+
+    await folder.save();
+    await Todo.findOneAndDelete({ _id: id });
+
+    res.status(200).json({
+      isError: false,
+      message: "Successfully deleted todo.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
+
+const checkIfUserHasFolder = (folderId, user, res) => {
+  if (!user.folders.includes(folderId.toString())) {
+    return res.status(400).json({
+      isError: true,
+      message: "No such folder",
+    });
+  }
+};
+
+// POST => /:id/done-todo
+exports.postDoneTodo = async (req, res) => {
+  const { id } = req.body;
+  const points = Math.floor(Math.random() * (11 - 5) + 5);
+  req.user.points += points;
+  req.user.totalCompletedTodos += 1;
+  try {
+    const todo = await Todo.findOne({ _id: id });
+    todo.isCompleted = true;
+
+    await req.user.save();
+    await todo.save();
+
+    res.status(200).json({
+      isError: false,
+      message: "Completed.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
+
+// POST => /:id/important-todo
+exports.postImportantTodo = async (req, res) => {
+  const { id } = req.body;
+  try {
+    const todo = await Todo.findOne({ _id: id });
+    todo.isImportant = !todo.isImportant;
+
+    await todo.save();
+
+    res.status(200).json({
+      isError: false,
+      message: "Marked as important.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      isError: true,
+      message: "Something went wrong...",
+    });
+  }
+};
--- server/routes/folder.js
+++ server/routes/folder.js
@@ -11,7 +11,25 @@ const FolderController = require("../controllers/folder");
 // GET => /
 router.get("/", verifyUser, FolderController.getFolders);
 
+// GET => /:id
+router.get("/:id", verifyUser, FolderController.getTodos);
+
 // POST => /create-folder
 router.post("/create-folder", verifyUser, FolderController.postCreateFolder);
+
+// POST => /:id/add-todo
+router.post("/:id/add-todo", verifyUser, FolderController.postAddTodo);
+
+// POST => /:id/delete-todo
+router.post("/:id/delete-todo", verifyUser, FolderController.postDeleteTodo);
+
+// POST => /:id/done-todo
+router.post("/:id/done-todo", verifyUser, FolderController.postDoneTodo);
+
+// POST => /:id/important-todo
+router.post("/:id/important-todo", verifyUser, FolderController.postImportantTodo);
+
+// GET => /:id/delete-folder
+router.get("/:id/delete-folder", verifyUser, FolderController.getDeleteFolder);
 
 module.exports = router;
